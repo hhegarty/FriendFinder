66c0063346244072fc84acf7c148aaba0e657d36
honora hegarty <honora.hegarty@verizon.net>
2020-01-18 21:28:27 -0500

    Adding friends array

---
+++ App/Data/friend.js
@@ -0,0 +1,167 @@
+var friendsArray = [
+    {
+        name: 'Andy Biersack',
+        photo: "http://s8.favim.com/orig/150415/andy-biersack-black-veil-brides-bvb-Favim.com-2651245.jpg",
+            scores: [
+                '2',
+                '4',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '2',
+                '1',
+                '5'
+
+            ]
+    },
+
+    {
+        name: "Christian Comma",
+        photo:"https://vignette.wikia.nocookie.net/blackveilbrides/images/4/45/Christian-Coma-christian-coma-38352720-640-960.jpg/revision/latest/scale-to-width-down/340?cb=20150804204723",
+            scores: [
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1',
+                '1'
+
+            ]
+    },
+    {
+        name: "Jinxx",
+        photo:"https://www.thefamouspeople.com/profiles/images/jinxx-3.jpg",
+            scores: [
+                '1',
+                '5',
+                '5',
+                '1',
+                '5',
+                '5',
+                '1',
+                '1',
+                '1',
+                '5'
+            ]
+    },
+    {
+        name: "Jake Pitts",
+        photo:"https://i.pinimg.com/originals/dc/4d/8c/dc4d8c0b7234262e3fd5435a52d4d0dd.jpg",
+            scores: [
+                '5',
+                '1',
+                '2',
+                '1',
+                '3',
+                '4',
+                '1',
+                '3',
+                '3',
+                '4'
+            ]
+    },
+    {
+        name: "Ashley Purdy",
+        photo:"http://images5.fanpop.com/image/photos/26200000/Ashley-Purdy-black-veil-brides-26287911-414-640.jpg",
+            scores: [
+                '1',
+                '1',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '1'
+            ]
+    },
+    {
+        name: "Lonny Eagleton",
+        photo:"https://pbs.twimg.com/profile_images/1200278315858194435/HgSDD7Q6_400x400.jpg",
+            scores: [
+                '1',
+                '1',
+                '5',
+                '1',
+                '3',
+                '2',
+                '5',
+                '1',
+                '1',
+                '5'
+            ]
+    },
+    {
+        name: "Brendon Urie",
+        photo:"https://vignette.wikia.nocookie.net/taylor-swift/images/b/b6/Brendon_Urie.jpeg/revision/latest?cb=20190426025509",
+            scores: [
+                '5',
+                '4',
+                '4',
+                '2',
+                '4',
+                '4',
+                '3',
+                '2',
+                '3',
+                '3'
+            ]
+    },
+    {
+        name: "Jared Leto",
+        photo: "https://indie-mag.com/wp-content/uploads/2017/12/jaredleto_final-1-copysmall.jpg",
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: "Shannon Leto",
+        photo:"http://img4.bdbphotos.com/images/500x250/p/9/p9a6moff0uyluf.jpg?skj2io4l",
+            scores: [
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5',
+                '5'
+            ]
+    },
+    {
+        name: "Tomo Miličević",
+        photo:"http://image.pollstar.com/WeblogFiles/pollstar/1312131202439946113_v1.jpg",
+            scores: [
+                '5',
+                '2',
+                '3',
+                '1',
+                '5',
+                '1',
+                '3',
+                '1',
+                '1',
+                '5'
+            ]
+    }
+];
+
+module.exports = friendsArray;
